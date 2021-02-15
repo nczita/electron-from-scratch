7fb3829f8af3cbd85cbe8c94cd8e72e754bcabd8
Łukasz Dziewanowski <lukasz.dziewanowski@gmail.com>
2021-02-15 20:35:04 +0100

    Window with content and icon

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,11 +1,16 @@
 const { app, BrowserWindow } = require("electron");
 
+let mainWindow;
+
 function createMainWindow() {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: "ImageShrink",
     width: 500,
     height: 600,
+    icon: "./assets/icons/favicon-64x64.png",
   });
+
+  mainWindow.loadFile("./app/index.html");
 }
 
 app.on("ready", createMainWindow);
