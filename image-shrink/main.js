9b68d4295c24e02502212f6d41beacf1d95f6831
Łukasz Dziewanowski <lukasz.dziewanowski@gmail.com>
2021-02-16 14:11:04 +0100

    Environment and platform check

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,5 +1,10 @@
 const { app, BrowserWindow } = require("electron");
 
+process.env.NODE_ENV = "development";
+
+const isMac = process.platform === "darwin";
+const isDev = process.env.NODE_ENV !== "production";
+
 let mainWindow;
 
 function createMainWindow() {
@@ -8,9 +13,22 @@ function createMainWindow() {
     width: 500,
     height: 600,
     icon: "./assets/icons/favicon-64x64.png",
+    resizable: isDev,
   });
 
   mainWindow.loadFile("./app/index.html");
 }
 
 app.on("ready", createMainWindow);
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
