3ad0088fecc88e1084ab20b87d02799aaf3016fa
Łukasz Dziewanowski <lukasz.dziewanowski@gmail.com>
2021-02-22 14:23:15 +0100

    First constant updates

--- sys-top/app/js/monitor.js
+++ sys-top/app/js/monitor.js
@@ -12,3 +12,25 @@ document.getElementById("os").innerText = `${os.type()} ${os.arch()}`;
 mem.info().then((info) => {
   document.getElementById("mem-total").innerText = info.totalMemMb;
 });
+
+setInterval(() => {
+  cpu.usage().then((info) => {
+    document.getElementById("cpu-usage").innerText = info + "%";
+  });
+
+  cpu.free().then((info) => {
+    document.getElementById("cpu-free").innerText = info + "%";
+  });
+
+  document.getElementById("sys-uptime").innerText = secondsToDhms(os.uptime());
+}, 2000);
+
+function secondsToDhms(seconds) {
+  seconds = +seconds;
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+
+  return `${d}d, ${h}h, ${m}m, ${s}s`;
+}
