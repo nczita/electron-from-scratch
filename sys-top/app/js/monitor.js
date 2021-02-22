9d7fe643d6665ccd40e567fc3145fc9800a410d2
Łukasz Dziewanowski <lukasz.dziewanowski@gmail.com>
2021-02-22 14:39:39 +0100

    Nofication with frequency interval

--- sys-top/app/js/monitor.js
+++ sys-top/app/js/monitor.js
@@ -6,6 +6,7 @@ const mem = osu.mem;
 const os = osu.os;
 
 let cpuOverload = 5;
+let alertFrequency = 1;
 
 document.getElementById("cpu-model").innerText = cpu.model();
 document.getElementById("comp-name").innerText = os.hostname();
@@ -13,12 +14,6 @@ document.getElementById("os").innerText = `${os.type()} ${os.arch()}`;
 
 mem.info().then((info) => {
   document.getElementById("mem-total").innerText = info.totalMemMb;
-});
-
-notifyUser({
-  title: "CPU Overload",
-  body: `CPU is over ${cpuOverload}`,
-  icon: path.join(__dirname, "img", "icon.png"),
 });
 
 setInterval(() => {
@@ -30,6 +25,16 @@ setInterval(() => {
       document.getElementById("cpu-progress").style.background = "red";
     } else {
       document.getElementById("cpu-progress").style.background = "#30c88b";
+    }
+
+    if (info >= cpuOverload && runNotify(alertFrequency)) {
+      notifyUser({
+        title: "CPU Overload",
+        body: `CPU is over ${cpuOverload}`,
+        icon: path.join(__dirname, "img", "icon.png"),
+      });
+
+      localStorage.setItem("lastNotify", +new Date());
     }
   });
 
@@ -52,4 +57,18 @@ function secondsToDhms(seconds) {
 
 function notifyUser(options) {
   new Notification(options.title, options);
+}
+
+function runNotify(frequency) {
+  if (localStorage.getItem("lastNotify") === null) {
+    localStorage.setItem("lastNotify", +new Date());
+    return true;
+  }
+
+  const notifyTime = new Date(parseInt(localStorage.getItem("lastNotify")));
+  const now = new Date();
+  const diffTime = Math.abs(now - notifyTime);
+  const minutesPassed = Math.ceil(diffTime / (1000 * 60));
+
+  return minutesPassed > frequency;
 }
